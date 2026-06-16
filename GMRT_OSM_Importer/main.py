5409cc9b22b626877c6e9a869259f223e2480e16
James Packer <james@bridgecommand.co.uk>
2026-06-16 19:11:33 +0100

    Revert use_osm_coastline change

--- GMRT_OSM_Importer/main.py
+++ GMRT_OSM_Importer/main.py
@@ -51,7 +51,7 @@ java_home = Path("openjdk-25.0.1_windows-x64_bin/jdk-25.0.1")
 
 # If we want to add coastline detail with the OSM coastline data
 # Warning, this is currently very slow for reasonable size output samples
-use_osm_coastline = False
+use_osm_coastline = True
 
 # The OSM coastlines (land polygons) file to use
 # This can be downloaded from
